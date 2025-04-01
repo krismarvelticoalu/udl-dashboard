4266c4778fe3c4234954fd245376a14dd1b8bc27
avelbryl <avelticoalu22@gmail.com>
2025-04-01 23:02:41 +0800

    Fix checkbox on column's name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,6 @@
 <x-layout>
     <div class="flex flex-col md:flex-row h-screen">
         <!-- Sidebar - Collapsible on mobile -->
-        <!-- <aside class="w-full md:w-64 bg-white p-4 md:p-6 flex md:flex-col justify-between shadow-md md:h-screen overflow-y-auto">
-            <div class="flex md:flex-col w-full">
-                <div class="flex items-center space-x-2 mb-4 md:mb-10">
-                    <div class="w-8 h-8 bg-gray-300 rounded-full"></div>
-                    <span class="text-gray-700 font-medium">Product Admin</span>
-                </div>
-                <nav class="flex-1 md:mt-0 flex justify-center md:justify-start">
-                    <a href="#" class="flex items-center text-[#90C853] font-medium">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="#90C853">
-                            <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                        </svg>
-                        Products
-                    </a>
-                </nav>
-            </div>
-            
-            <div class="hidden md:block">
-                <button class="border border-[#90C853] text-[#90C853] py-2 px-6 rounded-full flex items-center justify-center w-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="#90C853">
-                        <path fill-rule="evenodd" d="M3 10a1 1 0 011-1h8.586l-3.293-3.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 11-1.414-1.414L12.586 11H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-                    </svg>
-                    <span class="font-medium text-sm">Log out</span>
-                </button>
-            </div>
-        </aside> -->
-        <!-- Sidebar - Collapsible on mobile -->
         <aside class="w-full md:w-64 bg-white p-4 md:p-6 flex md:flex-col justify-between shadow-md md:h-screen overflow-y-auto">
             <div class="flex md:flex-col w-full">
                 <div class="flex items-center space-x-2 mb-4 md:mb-10">
@@ -102,9 +76,7 @@
                 <table class="w-full text-left border-collapse min-w-full">
                     <thead>
                         <tr class="bg-[#E3F2D3]">
-                            <th class="p-3 md:p-4 pl-4 md:pl-6 w-12">
-                                <input type="checkbox" class="rounded">
-                            </th>
+                            <th class="p-3 md:p-4 pl-4 md:pl-6 w-12"></th>
                             <th class="p-3 md:p-4 text-sm font-medium text-gray-600">Product Name</th>
                             <th class="p-3 md:p-4 text-sm font-medium text-gray-600">Category</th>
                             <th class="p-3 md:p-4 text-sm font-medium text-gray-600">Price</th>
